de8b66c0e1175b54abc45fdb228f27181bfb2d64
LFDM <1986gh@gmail.com>
2019-06-23 14:04:53 +0200

    Add first test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { parseSitemapFromString } from '.';
+
+const BASE = path.join(__dirname, 'examples');
+const readFile = (p: string) => fs.readFileSync(p).toString();
+
+const sitemap = readFile(path.join(BASE, 'sitemap.xml'));
+
 describe('parseSitemapsFromString', () => {
   it('runs', () => {
-    expect(true).toBe(true);
+    return parseSitemapFromString('http//example.com', sitemap).then(pages => {
+      console.log(pages);
+      expect(true).toBe(true);
+    });
   });
 });
