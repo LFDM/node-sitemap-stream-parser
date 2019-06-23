a2dedb9c432eae584d758b308af84c0f6be8d0c4
LFDM <1986gh@gmail.com>
2019-06-23 15:20:39 +0200

    Make spec more meaningful

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -10,8 +10,20 @@ const sitemap = readFile(path.join(BASE, 'sitemap.xml'));
 describe('parseSitemapsFromString', () => {
   it('runs', () => {
     return parseSitemapFromString('http//example.com', sitemap).then(pages => {
-      console.log(pages);
-      expect(true).toBe(true);
+      expect(pages).toHaveLength(2);
+
+      const [p1, p2] = pages;
+      expect(p1.url).toEqual(
+        'https://notanomadblog.com/amalfi-coast-budget-tips/'
+      );
+      expect(p1.lastModified).toEqual('2019-06-21T21:14:47+02:00');
+      expect(p1.sitemapUrl).toEqual('http//example.com');
+
+      expect(p2.url).toEqual(
+        'https://notanomadblog.com/best-day-trips-from-cape-town/'
+      );
+      expect(p2.lastModified).toEqual('2019-06-22T10:36:55+02:00');
+      expect(p2.sitemapUrl).toEqual('http//example.com');
     });
   });
 });
