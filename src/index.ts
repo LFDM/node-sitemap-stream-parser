ded7f45e099d423ffa806db666dd67d704e223a2
LFDM <1986gh@gmail.com>
2019-06-23 13:45:48 +0200

    Keep track of already parsed sitemaps

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import request from 'request';
 import sax from 'sax';
 import { Stream } from 'stream';
-import url from 'url';
 import { IPage } from './types';
+
+type SitemapIndex = { [url: string]: boolean };
 
 const flatten = <T>(lists: T[][]): T[] =>
   lists.reduce((r, l) => r.concat(l), []);
@@ -24,27 +25,35 @@ request.defaults({
   timeout: parseInt(process.env.SITEMAP_PARSER_TIMEOUT || '', 10) || 60000
 });
 
-export const parseSitemapFromUrl = (url: string): Promise<IPage[]> => {
+export const parseSitemapFromUrl = (
+  url: string,
+  sitemapIndex: SitemapIndex = {}
+): Promise<IPage[]> => {
   return new Promise((resolve, reject) => {
     const stream = request.get(url, { gzip: true });
     stream.on('error', reject);
-    return parseSitemap(url, stream).then(resolve);
+    return parseSitemap(url, stream, sitemapIndex).then(resolve);
   });
 };
 
 export const parseSitemapsFromUrls = (urls: string[]) => {
-  url.resolve;
-  return Promise.all(urls.map(parseSitemapFromUrl)).then(flatten);
+  const index: SitemapIndex = {};
+  return Promise.all(urls.map(url => parseSitemapFromUrl(url, index))).then(
+    flatten
+  );
 };
 
 export const parseSitemapFromString = (baseUrl: string, xml: string) => {
-  return parseSitemap(baseUrl, toReadableStream(xml));
+  return parseSitemap(baseUrl, toReadableStream(xml), {});
 };
 
 export const parseSitemap = (
   baseUrl: string,
-  xmlStream: Stream
+  xmlStream: Stream,
+  visitedSitemaps: SitemapIndex
 ): Promise<IPage[]> => {
+  visitedSitemaps[baseUrl] = true;
+
   return new Promise((resolve, reject) => {
     const parserStream = sax.createStream(false, {
       trim: true,
