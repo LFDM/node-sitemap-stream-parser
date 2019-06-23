a753bda92793c703812952810726183006e1fd7d
LFDM <1986gh@gmail.com>
2019-06-23 13:17:31 +0200

    Add parseSitemaps method

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { IPage } from './types';
 
-export const parseSitemap = (): Promise<IPage[]> => {
+const flatten = <T>(lists: T[][]): T[] =>
+  lists.reduce((r, l) => r.concat(l), []);
+
+export const parseSitemap = (url: string): Promise<IPage[]> => {
   return Promise.resolve([]);
+};
+
+export const parseSitemaps = (urls: string[]): Promise<IPage[]> => {
+  return Promise.all(urls.map(parseSitemap)).then(flatten);
 };
