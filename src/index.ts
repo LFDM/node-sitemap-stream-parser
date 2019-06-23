4f0a4848a88f6d12e0ee573fad1c185419eccfa3
LFDM <1986gh@gmail.com>
2019-06-23 14:52:23 +0200

    Implement first run through a sitemap

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import request from 'request';
 import sax from 'sax';
 import { Stream } from 'stream';
+import urlParser from 'url';
 import { IPage } from './types';
 
 type SitemapIndex = { [url: string]: boolean };
@@ -47,6 +48,12 @@ export const parseSitemapFromString = (baseUrl: string, xml: string) => {
   return parseSitemap(baseUrl, toReadableStream(xml), {});
 };
 
+const emptyPage = (baseUrl: string): IPage => ({
+  url: '',
+  lastModified: '',
+  src: baseUrl
+});
+
 export const parseSitemap = (
   baseUrl: string,
   xmlStream: Stream,
@@ -55,10 +62,89 @@ export const parseSitemap = (
   visitedSitemaps[baseUrl] = true;
 
   return new Promise((resolve, reject) => {
+    const state = {
+      url: false,
+      loc: false,
+      lastmod: false,
+      isSitemap: false,
+      isSitemapIndex: false,
+
+      currentPage: emptyPage(baseUrl),
+      pages: [] as IPage[],
+
+      sitemaps: [] as string[]
+    };
+
     const parserStream = sax.createStream(false, {
       trim: true,
       normalize: true,
       lowercase: true
     });
+
+    parserStream.on('opentag', node => {
+      if (node.name === 'url') {
+        state.url = true;
+      }
+
+      if (node.name === 'loc') {
+        state.loc = true;
+      }
+      if (node.name === 'lastmod') {
+        state.lastmod = true;
+      }
+      if (node.name === 'urlset') {
+        state.isSitemap = true;
+      }
+      if (node.name === 'sitemapindex') {
+        state.isSitemapIndex = true;
+      }
+    });
+
+    parserStream.on('closetag', tag => {
+      if (tag === 'url') {
+        state.url = false;
+        state.pages.push(state.currentPage);
+        state.currentPage = emptyPage(baseUrl);
+      }
+
+      if (tag === 'loc') {
+        state.loc = false;
+      }
+      if (tag == 'lastmod') {
+        state.lastmod = false;
+      }
+    });
+
+    parserStream.on('text', t => {
+      if (state.isSitemap) {
+        if (state.url) {
+          if (state.loc) {
+            state.currentPage.url = urlParser.resolve(baseUrl, t);
+          }
+          if (state.lastmod) {
+            state.currentPage.lastModified = t;
+          }
+        }
+      }
+
+      if (state.isSitemapIndex) {
+        if (state.loc) {
+          state.sitemaps.push(urlParser.resolve(baseUrl, t));
+        }
+      }
+    });
+
+    parserStream.on('error', reject);
+    parserStream.on('end', () => {
+      console.log('done'!);
+      if (state.isSitemapIndex) {
+        // iterate once more
+        return;
+      }
+
+      resolve(state.pages);
+    });
+
+    xmlStream.pipe(parserStream);
   });
 };
